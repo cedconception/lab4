1bf1be33db238185eda4505c84672bc5f2b162cf
CEDRIC MUTEYA <cedricarnoldmbulamuteya@gmail.com>
2023-04-13 22:48:30 +0200

    Addding a second function sum of arr in prog.c

--- prog.c
+++ prog.c
@@ -4,16 +4,27 @@
  * */
 
 #include <stdio.h>
-int a,m, min;
-printf("enter the number \n");
-scanf("%d",&min);
-do{
-scanf("%d",&a); 
-if((a<min)&&(a!=9))
-	min=a;
+// min function of two function 
+int  min(int a, int b){;
+	return (a > b ) ? b : a;
 }
-while(a!=9);
+
+// sum of an arr
+int sum(int arr[], int len){
+	int sum=0;
+    for(int i=0; i<len; i++){
+         sum+=arr[i];     
+    }
+ 	return sum;
+}
+
 int main(){
-	printf("This is a c file created after learning git"); 
-	printf("the min is: %d",min);
+	printf("This is a c file created after learning git \n\n"); 
+
+	printf("the min of 5 et 7 is: %d \n",min(5,9));
+	
+	int arr[] = {2, 4, 6};
+	printf("arr sum in our prog is: %d",sum(arr,3));
+
+
 }
