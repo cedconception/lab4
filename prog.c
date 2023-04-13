a873b969ff92c6d35c97e11bc490f7d923a12de1
MUNGUAHANA <120721567+MUNGUAHANA@users.noreply.github.com>
2023-04-13 16:22:43 +0100

    this function will find the min

--- prog.c
+++ prog.c
@@ -4,6 +4,16 @@
  * */
 
 #include <stdio.h>
+int a,m, min;
+printf("enter the number \n");
+scanf("%d",&min);
+do{
+scanf("%d",&a); 
+if((a<min)&&(a!=9))
+	min=a;
+}
+while(a!=9);
 int main(){
 	printf("This is a c file created after learning git"); 
+	printf("the min is: %d",min);
 }
