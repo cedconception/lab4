b0ca3e1d7d6330d9cc645f693ac4ce2615b6ba32
CEDRIC MUTEYA <cedricarnoldmbulamuteya@gmail.com>
2023-04-13 17:09:56 +0200

    I ve just created the prog.c file for Practice

---
+++ prog.c
@@ -0,0 +1,9 @@
+/*
+ * @authors : Kambale Muteya Cedric & Munguahana Bujiriri David
+ * file : prog.c
+ * */
+
+#include <stdio.h>
+int main(){
+	printf("This is a c file created after learning git"); 
+}
